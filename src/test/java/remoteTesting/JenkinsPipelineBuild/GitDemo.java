a47d5215a65715cbc6746b5afed22a14a04896fd
Arpit <arpitspawar55@gmail.com>
2021-10-23 17:07:45 +0530

    first commit

---
+++ src/test/java/remoteTesting/JenkinsPipelineBuild/GitDemo.java
@@ -0,0 +1,12 @@
+package remoteTesting.JenkinsPipelineBuild;
+
+import org.testng.annotations.Test;
+
+public class GitDemo {
+
+	@Test
+	private void BuildDemo() {
+		System.out.println("Jenikins file Git Demo successfully printed");
+	}
+
+}
